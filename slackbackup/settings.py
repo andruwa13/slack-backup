1fea77827f700b50b750eb03b617f0ae63edc5d8
maksim <nagaev.maksim@gmail.com>
2016-10-29 18:23:59 +0300

    Remove heroku deps

--- slackbackup/settings.py
+++ slackbackup/settings.py
@@ -96,8 +96,8 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.Loader',
 )
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'].update(dj_database_url.config())
+# import dj_database_url
+# DATABASES['default'].update(dj_database_url.config())
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
