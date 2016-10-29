0c9c51e63d93ac5f7a6177f85e43644defe19bfc
maksim <nagaev.maksim@gmail.com>
2016-10-29 15:50:02 +0300

    Improve django admin for troubleshooting

--- backupdata/admin.py
+++ backupdata/admin.py
@@ -1,7 +1,22 @@
 from backupdata.models import Channel, Message, ChannelMember
 from django.contrib import admin
 
+
+def get_history(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.crawl_history(True)
+get_history.short_description = "Download history"
+
+class ChannelAdmin(admin.ModelAdmin):
+    actions = [get_history]
+    list_display = ("name", "next_crawl_time", "latest_crawled", "oldest_crawled" ,"get_creator" )
+    ordering = ('name',)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_filter = ("channel", )
+    ordering = ('-ts',)
+
 # Register your models here.
-admin.site.register(Channel)
-admin.site.register(Message)
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Message, MessageAdmin)
 admin.site.register(ChannelMember)
